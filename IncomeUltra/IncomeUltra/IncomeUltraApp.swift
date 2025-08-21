ade7e2c07349d54412641e7413384b7a62d861f1
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-08-21 18:47:21 +0700

    Initial Xcode Project

---
+++ IncomeUltra/IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by Dylan on 21/8/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
