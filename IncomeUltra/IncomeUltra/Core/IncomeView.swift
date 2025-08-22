efd94705d76922d9e6a73fd2c80317711c21bbc6
Pyae Sone Han <pyaesonehan246810@gmail.com>
2025-08-22 17:16:50 +0700

    Create IncomeViewModel & Add Title to IncomeView

--- IncomeUltra/IncomeUltra/Core/IncomeView.swift
+++ IncomeUltra/IncomeUltra/Core/IncomeView.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct IncomeView: View {
+    @State private var viewModel: IncomeViewModel = IncomeViewModel()
     var body: some View {
-        Text("Income")
+        content
+            .navigationBarTitleDisplayMode(.large)
+            .toolbar {
+                toolbarContent
+            }
+    }
+}
+
+private extension IncomeView {
+    var content: some View {
+        ScrollView {
+            VStack(spacing: 16.0) {
+                
+            }
+        }
+    }
+    @ToolbarContentBuilder
+    var toolbarContent: some ToolbarContent {
+        ToolbarItem(placement: .principal) {
+            HStack(spacing: 4.0) {
+                Image(systemName: "dollarsign.circle")
+                    .foregroundStyle(Color.appColorTheme.accent)
+                Text(viewModel.appName)
+            }
+            .fontWeight(.semibold)
+        }
     }
 }
 
 #Preview {
-    IncomeView()
+    NavigationStack {
+        IncomeView()
+    }
 }
---
+++ IncomeUltra/IncomeUltra/Core/IncomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  IncomeViewModel.swift
+//  IncomeUltra
+//
+//  Created by Dylan on 22/8/25.
+//
+
+import Observation
+import Factory
+
+@MainActor
+@Observable
+final class IncomeViewModel {
+    @ObservationIgnored @Injected(\.incomeStore) private var incomeStore
+    @ObservationIgnored @Injected(\.appInfoStore) private var appInfoStore
+    
+    var appName: String {
+        appInfoStore.name
+    }
+}
